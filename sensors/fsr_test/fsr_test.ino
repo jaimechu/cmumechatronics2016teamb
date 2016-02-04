b34c7f449aa67d2559fa20b143db99a16b927b01
npol <nishant_pol1@yahoo.com>
2016-02-03 21:17:32 -0500

    Sensor Lab Demo code

--- sensors/fsr_test/fsr_test.ino
+++ sensors/fsr_test/fsr_test.ino
@@ -5,19 +5,19 @@ left most pin and right most pin as B and D */
 const int ptD = A0; // leftmost side
 const int ptB = A1; // right most sides
 int sensorValue = 0; 
+
 int sensorB, sensorD = 10;
 
 void setup() {
   Serial.begin(9600);
   pinMode(sensorB, INPUT);
   pinMode(sensorD, INPUT);
-  
 }
 
 void loop(){
   sensorB = analogRead(ptB);
   sensorD = analogRead(ptD);
-  sensorValue = sensorB - sensorD; 
+  sensorValue = sensorD - sensorB; 
   Serial.print("sensor = ");
   Serial.println(sensorValue);
   Serial.print(sensorB);
